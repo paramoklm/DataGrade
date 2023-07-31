10d815889bed156a9ead17e61ba2bd793f82c77b
Param Dave <param2001.dave@gmail.com>
2023-07-31 16:00:19 +0200

    feat(front-home): navbar border

---
+++ front-datagrade/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='w-full bg-gray-500 top-0 h-12 absolute border-slate-400 border-4'></div>
+    );
+};
+
+export default NavBar;
--- front-datagrade/src/pages/Home.js
+++ front-datagrade/src/pages/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Arc from "./../components/Arc"
 import Calculator from '../components/Home/Calculator';
+import NavBar from '../components/NavBar';
 
 const Home = () => {
     return (
         <div className='w-screen h-screen bg-gray-600'>
+            <NavBar/>
             <Arc/>
             <Calculator/>
         </div>
