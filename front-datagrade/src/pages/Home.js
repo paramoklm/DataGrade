746771dca4725b6210b25e08d63ccb65f1b69041
Param Dave <param2001.dave@gmail.com>
2023-07-31 15:42:34 +0200

    feat(front-home): add arc

--- front-datagrade/src/pages/Home.js
+++ front-datagrade/src/pages/Home.js
@@ -4,7 +4,8 @@ import Calculator from "./../assets/calculator-g712ba535e_1280.png"
 const Home = () => {
     return (
         <div className='w-screen h-screen bg-gray-600'>
-            <img className="relative h-3/5 left-1/4 top-1/3 absolute rotate-[14.57deg]" src={Calculator} alt="calculator"/>
+            <div className='w-3/5 h-4/5 bottom-0 rounded-bl-lg rounded-full absolute bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500'></div>
+            <img className="relative w-1/4 h-3/5 left-1/4 top-1/3 rotate-[14.57deg]" src={Calculator} alt="calculator"/>
         </div>
     );
 };
