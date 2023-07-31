4792046b4c3edc71154c43752b41a4a45b476465
Param Dave <param2001.dave@gmail.com>
2023-07-31 16:35:04 +0200

    feat(front-navbar): alternate home/about

--- front-datagrade/src/App.js
+++ front-datagrade/src/App.js
@@ -1,11 +1,16 @@
-// import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- front-datagrade/src/components/NavBar.js
+++ front-datagrade/src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+    const location = useLocation();
+    const isAboutPage = location.pathname === "/about";
     return (
-        <div className='w-full bg-gray-500 top-0 h-12 absolute border-slate-400 border-4'></div>
+        <div className='w-full bg-gray-500 top-0 h-12 absolute border-slate-400 border-4 flex'>
+            <ul className='w-1/12 right-0 ml-auto'>
+                <NavLink to={isAboutPage ? "/" : "/about"} className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                    <li className='text-2xl font-bold italic'>{isAboutPage? "Home" : "About"}</li>
+                </NavLink>
+            </ul>
+        </div>
     );
 };
 
