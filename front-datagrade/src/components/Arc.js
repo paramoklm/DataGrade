54a075150a8851ae0baa4ff56b06a24b61e72945
Param Dave <param2001.dave@gmail.com>
2023-07-31 15:51:36 +0200

    feat(front-home): reformat in component

---
+++ front-datagrade/src/components/Arc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Arc = () => {
+    return (
+        <div className='w-3/5 h-4/5 bottom-0 rounded-bl-lg rounded-full absolute bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500'></div>
+    );
+};
+
+export default Arc;
---
+++ front-datagrade/src/components/Home/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CalculatorImg from "./../../assets/calculator-g712ba535e_1280.png"
+
+const Calculator = () => {
+    return (
+        <img className="relative w-1/4 h-3/5 left-1/4 top-1/3 rotate-[14.57deg]" src={CalculatorImg} alt="calculator"/>
+    );
+};
+
+export default Calculator;
